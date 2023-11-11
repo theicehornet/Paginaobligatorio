af5286bbd728bf2cce9e34980db2d0ee81bca466
Alejandro <theicehornet1@gmail.com>
2023-11-11 15:15:09 -0500

    Cambiado nombre de MiembrosController a UsuariosController y creados metodos de accion de ver perfil y amigos y agregado màs tiempo a la duracion de la sesión.

---
+++ PAGINA_OBLIGATORIO/Controllers/UsuariosController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema;
+
+namespace PAGINA_OBLIGATORIO.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        RedSocial redsocial = RedSocial.Instancia;
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        #region AccionesDeAdmin
+        public IActionResult VisualizarMiembros()
+        {
+            if (HttpContext.Session.GetString("rol") != "admin")
+                return RedirectToAction("Login","Home");
+            ViewBag.Usuarios = redsocial.CopiadeListaMiembros();
+            return View("VerMiembros");
+        }
+        
+        public IActionResult VisualizarMiembrosOrdenados()
+        {
+            if (HttpContext.Session.GetString("rol") != "admin")
+                return RedirectToAction("Login", "Home");
+            List<Miembro> miembros = redsocial.CopiadeListaMiembros();
+            miembros.Sort();
+            ViewBag.Usuarios = miembros;
+            return View("VerMiembros");
+        }
+
+        public IActionResult Bloquear(string email)
+        {
+            if (HttpContext.Session.GetString("rol") != "admin")
+                return RedirectToAction("Login", "Home");
+            Miembro mi = redsocial.BuscarMiembro(email);
+            mi.Bloqueado = true;
+            return RedirectToAction("VisualizarMiembros");
+        }
+
+        public IActionResult Desbloquear(string email)
+        {
+            if (HttpContext.Session.GetString("rol") != "admin")
+                return RedirectToAction("Login", "Home");
+            Miembro mi = redsocial.BuscarMiembro(email);
+            mi.Bloqueado = false;
+            return RedirectToAction("VisualizarMiembros");
+        }
+        #endregion
+        #region AccionesDeMiembro
+        public IActionResult Amigos()
+        {
+            if (HttpContext.Session.GetString("rol") != "miembro")
+                return RedirectToAction("Login", "Home");
+            Miembro mi = redsocial.BuscarMiembro(HttpContext.Session.GetString("correo"));
+            ViewBag.Amigos = redsocial.GetAmigos(mi);
+            return View();
+        }
+
+        public IActionResult PerfilMiembro(string correo)
+        {
+            if (HttpContext.Session.GetString("rol") == null)
+                return RedirectToAction("Login", "Home");
+            Miembro buscado = redsocial.BuscarMiembro(correo);
+            ViewBag.Posts = redsocial.BuscarPostsdeMiembro(buscado);
+            return View("Perfil", buscado);
+        }
+
+        public IActionResult Perfil()
+        {
+            if (HttpContext.Session.GetString("rol") != "miembro")
+                return RedirectToAction("Login", "Home");
+            string correo = HttpContext.Session.GetString("correo");
+            Miembro logueado = redsocial.BuscarMiembro(correo);
+            ViewBag.Posts = redsocial.BuscarPostsdeMiembro(logueado);
+            return View(logueado);
+        }
+
+        
+        #endregion
+
+    }
+}
--- PAGINA_OBLIGATORIO/Program.cs
+++ PAGINA_OBLIGATORIO/Program.cs
@@ -12,7 +12,7 @@ namespace PAGINA_OBLIGATORIO
 
             builder.Services.AddSession(options =>
             {
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.IdleTimeout = TimeSpan.FromSeconds(500);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
